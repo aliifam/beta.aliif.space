34c195af62add6b2980a1494c65c4ceb8e982072
Aliif Arief <aliif.arief.m@gmail.com>
2022-01-09 07:29:19 +0700

    add animation

--- src/components/background.js
+++ src/components/background.js
@@ -6,9 +6,9 @@ export default function Background() {
     return (
         <div className="bg-line h-screen flex">
             <div className="container max-w-4xl h-screen mx-auto flex items-center">
-                <div className="container flex flex-col mx-4 lg:mx-0">
+                <div className="container flex flex-col mx-4 lg:mx-0" data-aos="fade-up" data-aos-delay="200">
                     <h1 className="font-bold font-serif text-5xl sm:text-6xl mb-4">Aliif Arief</h1>
-                    <p className="font-serif text-2xl">Hello, you can call me Aliif and I am <a href="https://trpl.sv.ugm.ac.id/" target="_blank" rel="noopener noreferrer" className="underline font-bold">Software Engineering</a> student at Gadjah Mada University, I am Enjoyed coding with JavaScript and Python.</p>
+                    <p className="font-serif text-xl sm:text-2xl">Hello, you can call me Aliif and I am <a href="https://trpl.sv.ugm.ac.id/" target="_blank" rel="noopener noreferrer" className="underline font-bold">Software Engineering</a> student at Gadjah Mada University, I am Enjoyed coding with JavaScript and Python.</p>
                     <div className="grid grid-cols-6 max-w-xs mt-6">
                        <SocialIcon/>
                     </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+      delay: 50,
+    });
+  });
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
